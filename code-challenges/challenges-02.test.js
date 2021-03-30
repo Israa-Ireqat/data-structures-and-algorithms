cb5fe4795cc93c5f0a46e82a1b26a1d5105bf570
Israa-Ireqat <israa.ireqat333@gmail.com>
2021-03-30 23:43:34 +0300

    test3

--- code-challenges/challenges-02.test.js
+++ code-challenges/challenges-02.test.js
@@ -6,6 +6,7 @@ CHALLENGE 1 - Review
 
 Write a function named raisedToTheThird that takes in an array of numbers and returns a new array of each of those numbers raised to the 3rd power (hint: look up Math.pow()). Use forEach to solve this problem.
 ///Start here:
+I will start 
 ------------------------------------------------------------------------------------------------ */
 
 const raisedToTheThird = (arr) => {
